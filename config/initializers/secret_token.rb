f8be993d8ffdf6d732c43a61cd19db2cc50ee39a
Sanjay K Manchiganti <email2sanjay@yahoo.com>
2012-07-15 22:38:47 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyWaterIQ::Application.config.secret_token = '3514b67d1f8dbc3849aa303c191f71d0472fa10022cd90ac12acb764dcd96dd65d6d40b8041ae4bd95c5f25d174581413ba79d143728b824b79c5e17ca5af0d2'
