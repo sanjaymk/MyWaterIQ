6021588acacb584ff723d4f820c104bb1e4b3355
Sanjay Manchiganti <email2sanjay@yahoo.com>
2013-08-04 14:52:15 -0400

    daily files

--- app/controllers/meter_readings_controller.rb
+++ app/controllers/meter_readings_controller.rb
@@ -9,6 +9,21 @@ class MeterReadingsController < ApplicationController
      customer_names_for_alerts = Array.new;
 
 
+  Dir.mkdir("/tmp/daily_files") unless Dir.exist?("./tmp/daily_files")
+  Dir.chdir("/tmp/daily_files") do
+    
+    Net::FTP.open("ftp.fkaa.com") do |ftp|
+      ftp.passive = true
+      ftp.login("admin.mywateriq@fkaa.com","89jSAQ6E")
+      ftp.get("orc_meters.txt")
+    end
+    curr_date = Time.now.strftime("%Y_%m_%d")
+    FileUtils.mv('orc_meters.txt',"orc_meters_#{curr_date}.txt")
+
+
+    puts "File is downloaded successfully"
+  end
+
     # lines = []
     # IO.foreach( "daily_files/orc_meters_working_whole.txt" ) do |line|
     #   lines << line.split('\t').map { |v| v.strip }
@@ -22,7 +37,7 @@ class MeterReadingsController < ApplicationController
     # File.open("daily_files/orc_meters_working_parsed.txt","w") { |f| f.write(lines) } 
     #file_to_be_processed = 'daily_files/orc_meters_original1.txt';
   row_count = 0
-  Dir.foreach("#{Rails.root}/tmp/daily_files/") {|file_to_be_processed| puts "File is #{file_to_be_processed}"}
+  Dir.foreach("/tmp/daily_files/") {|file_to_be_processed| puts "File is #{file_to_be_processed}"}
   #Dir.foreach("daily_files") {|file_to_be_processed| logger.debug "File is #{file_to_be_processed}"}
   #Dir.glob("daily_files/*.txt") {|file_to_be_processed| logger.debug "File is #{file_to_be_processed}"
   Dir.glob("#{Rails.root}/tmp/daily_files/*.txt") { |file_to_be_processed|
